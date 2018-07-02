8cc79020d3d7e8935416b9f3823ea648a94fefd9
Tina Giska <tina.tylicka@gmail.com>
2018-07-02 13:15:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase', {
-    useMongoClient: true
-});
+mongoose.connect('mongodb://localhost/nodeappdatabase');
 
 //new user Schema
 const userSchema = new Schema({
